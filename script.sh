8f592f9ede7e892c048740d7fb0b4fd687b9c592
scatterp2 <31651804+scatterp2@users.noreply.github.com>
2022-12-01 11:11:35 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,5 @@
 sudo apt-get update
+sudo apt-get install screen
 sudo dpkg --configure -a
 sudo apt-get install -y openjdk-8-jre-headless
 wget https://download.yacy.net/yacy_v1.924_20210209_10069.tar.gz
